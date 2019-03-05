1bc732da98b838f5db7ad651e6212f283efa7e4a
sethbaldridge87 <sethbaldridge87@gmail.com>
2019-03-04 18:22:25 -0700

    Version 2

---
+++ models/index.js
@@ -0,0 +1,28 @@
+// Dependencies
+// =============================================================
+// var DataTypes = require('sequelize/lib/data-types');
+// This may be confusing but here Sequelize (capital) references the standard library
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references our connection to the DB.
+var sequelize = require("../config/connection.js");
+
+// Creates a "Chirp" model that matches up with DB
+var db = sequelize.define("User info", {
+  // id: {
+  //   primaryKey: true,
+  //   type: Sequelize.INTEGER,
+  //   allowNull: false
+  // },
+  name: Sequelize.STRING,
+  age: Sequelize.INTEGER,
+  weight: Sequelize.INTEGER,
+  height: Sequelize.STRING,
+  gender: Sequelize.STRING
+});
+console.log("User on index.js");
+console.log(db);
+// Syncs with DB
+db.sync();
+
+// Makes the Chirp Model available for other files (will also create a table)
+module.exports = db;
---
+++ public/js/script.js
@@ -0,0 +1,73 @@
+$.get("/api/all", function(data) {
+      console.log("Here's the data:");
+      console.log(data);
+
+  if (data.length !== 0) {
+
+    for (var i = 0; i < data.length; i++) {
+      var row = $("<div class='row'>");
+      row.addClass("user");
+      row.append("<h3 data-id=" + data[i].id + ">User:</h3>");
+      row.append("<p>Name: " + data[i].name + "</p>");
+      // row.append("<p>Age: " + data[i].age + "</p>");
+      // row.append("<p>Height: " + data[i].height + "</p>");
+      // row.append("<p>Weight: " + data[i].weight + "</p>");
+      row.append("<a href='user?user_id=" + data[i].id + "'><button class='btn'>Select User</button></a><br><br>");
+      row.append("<button class='delete_user btn'>Delete User</button>");
+      row.append("<hr>");
+      $("#users").prepend(row);
+    }
+  }
+});
+$(document).ready(function(){
+  $('#submit_user').on('click', function(){
+    event.preventDefault();
+    var newUser = {
+      name: $('#UName').val().trim(),
+      age: $('#UAge').val().trim(),
+      weight: $('#UWeight').val().trim(),
+      height: $('#UHeight').val().trim(),
+      gender: $('#UGender').val().trim()
+    };
+    console.log(newUser);
+    $.post("/api/new", newUser)
+      .then(function(data){
+        location.reload();
+      })
+  });
+  $('.delete_user').click(function(){
+    var deletedID = $(this).closest('div').find('h3').attr('data-id');
+    deleteUser(deletedID);
+  });
+  function deleteUser(user){
+    alert(user);
+    $.ajax({
+      method: "DELETE",
+      url: "/api/delete/" + user
+    })
+    .then(function() {
+      
+    });
+  }
+  var url = window.location.href;   
+  console.log(url);
+  var target = url.substr(url.indexOf('_id=') + 4);
+  console.log(target);
+  $.ajax({
+    method: "GET",
+    url: "/api/single/" + target
+  })
+  .then(function(data){
+      var row = $("<div class='row'>");
+      row.addClass("user");
+      row.append("<h3 data-id=" + data.id + ">User:</h3>");
+      row.append("<p>Name: " + data.name + "</p>");
+      row.append("<p>Age: " + data.age + "</p>");
+      row.append("<p>Height: " + data.height + "</p>");
+      row.append("<p>Weight: " + data.weight + "</p>");
+      row.append("<button class='delete_user btn'>Delete User</button>");
+      row.append("<hr>");
+      $("#single_user").append(row);
+  });
+  
+});
